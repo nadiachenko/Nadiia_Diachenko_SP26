3d7d95cdf613ff25d0f7c73b1b8b7092929694fb
unknown <nadiadiachenko93@gmail.com>
2026-06-10 21:25:21 +0300

    task3

--- Python-Course/Session_3/module_3.py
+++ Python-Course/Session_3/module_3.py
@@ -1,19 +1,31 @@
-# import time
+import time
+#from datetime import time
 from typing import List
 
 Matrix = List[List[int]]
 
 
 def task_1(exp: int):
-    pass
+    def power(n):
+        return n ** exp
+    return power
 
 
 def task_2(*args, **kwags):
-    pass
+    for value in args:
+        print(value)
+    for value in kwags.values():
+        print(value)
 
 
 def helper(func):
-    pass
+    def wrapper(*args, **kwargs):
+        print("Hi, friend! What's your name?")
+        result = func(*args, **kwargs)
+        print("See you soon!")
+        return result
+    return wrapper
+
 
 
 @helper
@@ -22,7 +34,14 @@ def task_3(name: str):
 
 
 def timer(func):
-    pass
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        run_time = end - start
+        print(f"Finished {func.__name__} in {run_time:.4f} secs")
+        return result
+    return wrapper
 
 
 @timer
@@ -31,8 +50,27 @@ def task_4():
 
 
 def task_5(matrix: Matrix) -> Matrix:
-    pass
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    transposed = [[0] * rows for _ in range(cols)]
+
+    for i in range(rows):
+        for j in range(cols):
+            transposed[j][i] = matrix[i][j]
+
+    return transposed
 
 
 def task_6(queue: str):
-    pass
+    balance = 0
+
+    for char in queue:
+        if char == '(':
+            balance += 1
+        elif char == ')':
+            balance -= 1
+
+        if balance < 0:
+            return False
+    return balance == 0
